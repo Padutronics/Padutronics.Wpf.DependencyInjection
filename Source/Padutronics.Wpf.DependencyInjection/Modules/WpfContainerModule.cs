e316d6c64b20e2dcd81fc01fef2fc463b833652f
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-27 12:52:00 +0200

    Register type Padutronics.Wpf.ViewModels.Windows.WindowState

--- Source/Padutronics.Wpf.DependencyInjection/Modules/WpfContainerModule.cs
+++ Source/Padutronics.Wpf.DependencyInjection/Modules/WpfContainerModule.cs
@@ -8,5 +8,7 @@ internal sealed class WpfContainerModule : IContainerModule
     public void Load(IContainerBuilder containerBuilder)
     {
         containerBuilder.For<IWindowFinder>().Use<WindowFinder>().SingleInstance();
+
+        containerBuilder.For<IWindowState>().Use<WindowState>().InstancePerDependency();
     }
 }
