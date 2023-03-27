a8df6c94b1c62c4fb0356e74bef62ef01937fa7d
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-27 12:49:00 +0200

    Add root container module

---
+++ Source/Padutronics.Wpf.DependencyInjection/Modules/WpfContainerModule.cs
@@ -0,0 +1,10 @@
+using Padutronics.DependencyInjection;
+
+namespace Padutronics.Wpf.DependencyInjection.Modules;
+
+internal sealed class WpfContainerModule : IContainerModule
+{
+    public void Load(IContainerBuilder containerBuilder)
+    {
+    }
+}
